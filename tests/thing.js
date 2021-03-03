b0e6b4c4cb32e3da485ad43b28a8d592332883e8
endless <73157473+endlessXD@users.noreply.github.com>
2021-03-03 13:40:15 -0800

    Delete thing.js

--- tests/thing.js
+++
@@ -1,6 +0,0 @@
-const string = "diep.io/#ffffffff";
-const str = "diep.io/#2617536700A5AC1C20F5EC"
-const regex = /(https:\/\/diep\.io|diep\.io)\/#.[a-z0-9]{21}$/i;
-
-console.log(regex.test(string));
-console.log(regex.test(str));
