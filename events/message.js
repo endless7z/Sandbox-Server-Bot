716b42553f296f7b5f9c213c01640ffb893445f6
endless <73157473+endlessXD@users.noreply.github.com>
2021-03-04 10:29:30 -0800

    Update message.js

--- events/message.js
+++ events/message.js
@@ -1,7 +1,21 @@
+const { id } = require('../config.json');
+
 module.exports = {
   run(message) {
     if (message.content.includes('diep.io/#') && message.channel.type !== 'dm') {
       return message.delete();
+    };
+
+    if (message.channel.id == id.channel[1]) {
+      message.react('✅').then(
+        ({ message }) => {
+          message.react('❌');
+        }
+      ).catch(
+        () => { }
+      );
+
+      return;
     };
   }
 };
