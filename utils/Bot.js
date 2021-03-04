b677f4bc51b390c25cbf210eda09f2921780c8f8
endless <73157473+endlessXD@users.noreply.github.com>
2021-03-03 16:14:20 -0800

    Update Bot.js

--- utils/Bot.js
+++ utils/Bot.js
@@ -1,79 +1,85 @@
 const { BotError } = require('./errors/BotError');
 
 class Bot {
-	static SendEmbed(message, settings = [
-		'Title', 'Color', 'Timestamp', 'Footer', 'Description'
-	], fields = [
-		{ name: undefined, value: undefined, inline: false }
-	]) {
+  static SendEmbed(message, settings = [
+    'Title', 'Color', 'Timestamp', 'Footer', 'Description'
+  ], fields = [
+    {
+      name: undefined,
+      value: undefined,
+      inline: false
+    }
+  ]) {
 
-		const embed = {
-			title: settings[0],
-			color: settings[1],
-			timestamp: settings[2],
+    const embed = {
+      title: settings[0],
+      color: settings[1],
+      timestamp: settings[2],
 
-			footer: {
-				text: settings[3]
-			},
+      footer: {
+        text: settings[3]
+      },
 
-			description: settings[4],
-			fields: fields
-		};
+      description: settings[4],
+      fields: fields
+    };
 
-		if (!settings[4]) {
-			delete embed.description;
-		};
+    if (!settings[4]) {
+      delete embed.description;
+    };
 
-		if (!settings[0]) {
-			delete embed.title;
-		};
+    if (!settings[0]) {
+      delete embed.title;
+    };
 
-		if (!settings.length) {
-			throw new BotError('No Embed Values Detected');
-		};
+    if (!settings.length) {
+      throw new BotError('No Embed Values Detected');
+    };
 
-		if (fields == 0) delete embed.fields;
+    if (fields == 0) delete embed.fields;
 
-		message.channel.send({ embed: embed }).catch(() => { });
-	};
+    message.channel.send({
+      embed: embed
+    }).catch(() => {});
+  };
 
-	static hex(string = '') {
-		var result = '';
-		for (var i = 0; i < string.length; i++) {
-			result += string.charCodeAt(i).toString(16);
-		};
+  static hex(string = '') {
+    var result = '';
+    for (var i = 0; i < string.length; i++) {
+      result += string.charCodeAt(i).toString(16);
+    };
 
-		return result;
-	};
+    return result;
+  };
 
-	static convert(string = '') {
-		const converted = [];
-		const array = string.split('');
-		
-		function reverse(s) {
-			return s.split('').reverse().join('');
-		};
+  static convert(string = '') {
+    const converted = [];
+    const array = string.split('');
 
-		array.map((i) => {
-			converted.push(reverse(this.hex(i)))
-		});
+    function reverse(s) {
+      return s.split('').reverse().join('');
+    };
 
-		return 'https://diep.io/#' + converted.join('');
-	};
+    array.map((i) => {
+      converted.push(reverse(this.hex(i)))
+    });
 
-	static remove(a = [], item) {
-		const index = a.indexOf(item);
+    return 'https://diep.io/#' + converted.join('');
+  };
 
-		if (index > -1) {
-			a.splice(index, 1);
-		};
-	};
+  static remove(a = [], item) {
+    const index = a.indexOf(item);
 
-	static cut(a = []) {
-    return a.sort().filter(function (item, pos, ary) {
-			return !pos || item != ary[pos - 1];
-		});
-	};
+    if (index > -1) {
+      a.splice(index, 1);
+    };
+  };
+
+  static cut(a = []) {
+    return a.sort().filter(function(item, pos, ary) {
+      return !pos || item != ary[pos - 1];
+    });
+  };
 };
 
 module.exports.Bot = Bot;
