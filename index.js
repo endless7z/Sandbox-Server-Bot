5ebc39b965daa75a8a9285afbc1cbf503d1ddb51
endless <73157473+endlessXD@users.noreply.github.com>
2021-03-10 18:03:43 -0800

    Update index.js

--- index.js
+++ index.js
@@ -6,54 +6,54 @@ const { blacklisted } = require('./data/blacklisted.json');
 const { Color } = require('./utils/Design');
 
 const client = new Discord.Client({
-	ws: {
-		properties: {
-			$browser: 'Discord iOS'
-		}
-	}
+  ws: {
+    properties: {
+      $browser: 'Discord iOS'
+    }
+  }
 });
 
 const commands = [];
 
 client.on('ready', () => {
-	console.log(Color.banner);
-	require('./events/ready').run(client, commands);
+  console.log(Color.banner);
+  require('./events/ready').run(client, commands);
 });
 
 client.on('message', (message) => {
-	require('./events/message').run(message);
+  require('./events/message').run(message);
 
-	if (!message.content.startsWith(prefix) || message.author.bot) return;
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-	const args = message.content.slice(prefix.length).trim().split(/ +/);
-	const command = args.shift().toLowerCase();
+  const args = message.content.slice(prefix.length).trim().split(/ +/);
+  const command = args.shift().toLowerCase();
 
-	if (blacklisted.includes(message.author.id)) {
-		Bot.SendEmbed(message,
-			['Error', 0xFF0000, new Date(), 'endless OP', 'You Have Been Blacklisted'], 0
-		);
+  if (blacklisted.includes(message.author.id)) {
+    Bot.SendEmbed(message,
+      ['Error', 0xFF0000, new Date(), 'endless OP', 'You Have Been Blacklisted'], 0
+    );
 
-		return;
-	};
+    return;
+  };
 
-	if (commands.includes(command)) {
-		require(`./commands/${command}`).run(message, args);
-	} else {
-		Bot.SendEmbed(message,
-			['Help', 'GOLD', new Date(), 'endless OP', `Try \`${prefix}help\` for more information.`], 0
-		);
-	};
+  if (commands.includes(command)) {
+    require(`./commands/${command}`).run(message, args);
+  } else {
+    Bot.SendEmbed(message,
+      ['Help', 'GOLD', new Date(), 'endless OP', `Try \`${prefix}help\` for more information.`], 0
+    );
+  };
 });
 
 client.on('messageUpdate', (OldMessage, NewMessage) => {
-	require('./events/message').run(NewMessage);
+  require('./events/message').run(NewMessage);
 });
 
 client.login(token).catch(() => {
-	Color.log('Invalid Token');
+  Color.log('Invalid Token');
 
-	setTimeout(() => {
-		client.destroy();
-		process.exit(1);
-	}, 1000);
+  setTimeout(() => {
+    client.destroy();
+    process.exit(1);
+  }, 1000);
 });
